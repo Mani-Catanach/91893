6911af3e8b985f447b5c7ad19e639687a4fb9d77
Mani-Catanach <169108548+Mani-Catanach@users.noreply.github.com>
2025-08-13 11:26:52 +1200

    Add files via upload

--- B_01_Pizza_Cost_Calc_v4.py
+++ B_01_Pizza_Cost_Calc_v4.py
@@ -323,7 +323,7 @@ def clean_filename(raw_filename):
             valid_filename = False
             error = ("Oops - your product name / filename is too long.  \n"
                      "Please provide an alternate filename (<= 19 characters) \n"
-                     "or press <enter> to default to FRC_yyyy_mm_ddd")
+                     "or press <enter> to default to PCC_yyyy_mm_ddd")
 
         # iterate through filename and check for invalid characters
         for letter in raw_filename:
@@ -332,19 +332,19 @@ def clean_filename(raw_filename):
                 error = ("I can't use the product name / proposed filename \n"
                          "as it has illegal characters.  Please \n"
                          "enter an alternate name for the first part \n"
-                         "of the file or press <enter> to default to FRC_yyyy_mm_dd")
+                         "of the file or press <enter> to default to PCC_yyyy_mm_dd")
                 break
 
         if valid_filename is False:
             print(error)
             raw_filename = input("\nPlease enter an alternate name for the start of the file: ")
 
-            # reset valid_filename so that it new name can be checked.
+            # reset valid_filename so that it's new name can be checked.
             valid_filename = True
 
             # put in default filename if users press <enter>
             if raw_filename == "":
-                raw_filename = "FRC"
+                raw_filename = "PCC"
 
         else:
             return raw_filename
